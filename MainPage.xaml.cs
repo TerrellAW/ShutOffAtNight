55f0a1f40f258c74b4aa86d998879e7087cfc966
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-12-20 00:43:53 -0700

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShutOffAtNight
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Handlers;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace ShutOffAtNight
+{
+    public static class MauiProgram
+    {
+        // Public static variables
+        public static int InputMinutes;
+        public static bool IsTimerRunning = false;
+        public static bool IsShuttingDown = false;
+
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
+                    handlers.AddHandler<Microsoft.Maui.IImage, IImageHandler>();
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
